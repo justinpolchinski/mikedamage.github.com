200d7a8bed6ba52ac1e180e779c756d4b615c443
Mike Green <mike.is.green@gmail.com>
2010-12-12 22:43:23 -0500

    brought the container width back up to the standard 950px so that I could have a sidebar

--- js/main.js
+++ js/main.js
@@ -4,9 +4,29 @@
  */
 
 //= require "jquery-1.4.4"
+//= require "jquery.tmpl"
 
 $(document).ready(function() {
-
+	var $twitterFeed = $('#twitter')
+		, twitterPipeURL = $twitterFeed.attr('data-pipeurl');
+	
+	$.ajax({
+		url: twitterPipeURL
+		, type: 'GET'
+		, dataType: 'json'
+		, success: function(json) {
+			$twitterFeed.find('.loading').remove();
+			var tweets = _(json.value.items).map(function(tweet) {
+				var date = new Date(tweet.pubDate);
+				return {
+					description: tweet.description
+					, date: date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear()
+				};
+			});
+			window.tweets = tweets;
+			$('#twitter-item').tmpl(tweets).appendTo($twitterFeed);
+		}
+	});
 });
 
 // vim: set sw=2 ts=2 syntax=jquery :
